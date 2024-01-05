f5e77b5c9aae7557b70d1038b36dbd5e9261f58e
Gustavo Rodrigues <Gustavorr001@gmail.com>
2024-01-05 11:43:00 -0300

    payment, one to one association

--- src/main/java/com/techgusta/project/config/TestConfig.java
+++ src/main/java/com/techgusta/project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.techgusta.project.entities.Category;
 import com.techgusta.project.entities.Order;
 import com.techgusta.project.entities.OrderItem;
+import com.techgusta.project.entities.Payment;
 import com.techgusta.project.entities.Product;
 import com.techgusta.project.entities.User;
 import com.techgusta.project.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
-
 }
