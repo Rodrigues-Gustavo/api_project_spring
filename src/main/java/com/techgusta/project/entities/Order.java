c79605d114b313ebbfc446238817d3c9bf1722c8
Gustavo Rodrigues <Gustavorr001@gmail.com>
2024-01-08 10:09:11 -0300

    subtotal and total methods

--- src/main/java/com/techgusta/project/entities/Order.java
+++ src/main/java/com/techgusta/project/entities/Order.java
@@ -102,6 +102,14 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	
+	public Double getTotal() {
+		double sum =  0.0;
+		for (OrderItem obj : items) {
+			sum += obj.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
